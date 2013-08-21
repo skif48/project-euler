8563023773c38cc2eb82d8e798ea33b13dfbc2ba
Igor Usenko <nmds48@gmail.com>
2013-08-21 19:02:34 +0300

    solving problem 024

--- test/fast/problems/problem024/LexicographicPermutationsGeneratorTest.java
+++ test/fast/problems/problem024/LexicographicPermutationsGeneratorTest.java
@@ -18,4 +18,16 @@ public class LexicographicPermutationsGeneratorTest {
         Assert.assertEquals(210, generator.next());
     }
 
+    /*
+    0123456789
+    0123456798
+    0123456879
+    0123456897
+    0123456978
+    0123456987
+    0123457689
+    0123457698
+    0123457869
+    0123457896
+     */
 }
